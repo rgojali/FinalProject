198efc8b5c2eac756d3a14d93d76864d4c5018a0
Rusilah Gojali <rgojali@gmail.com>
2019-02-04 01:29:13 -0800

    added files to folder

---
+++ src/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <header>Hello World</header>
+      </div>
+    );
+  }
+}
+
+export default Header;
