6c1793c517def8742f29f697fe21400dd2bcdb12
Rusilah Gojali <rgojali@gmail.com>
2019-02-09 23:25:02 -0800

    added node stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
-import { ProfilePage } from "./Profile/ProfilePage";
-import { NavBar } from "./Navbar/NavBar";
+import { ProfilePage } from "../Profile/ProfilePage";
+import { NavBar } from "../Navbar/NavBar";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return <div className="App" >;
-    <ProfilePage />
-    <NavBar />
-    </div>
-  }
+    render() {
+        return ( <
+            div className = "App" >
+
+            <
+            div className = "Profile" >
+            <
+            ProfilePage / >
+            <
+            /div>
+
+            <
+            div className = "NavBar" >
+            <
+            NavBar / >
+            <
+            /div>
+
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
